63c76144dbb787c6e246656b14744a646873a4e0
conyoung18 <conyoung@byu.edu>
2025-02-19 10:22:49 -0700

    Initial changes

--- Mission6_Young/Models/Movie.cs
+++ Mission6_Young/Models/Movie.cs
@@ -17,19 +17,19 @@ public class Movie
     [Key]
     [Required]
     public int MovieID { get; set; }
-    [Required]
-    public string Category { get; set; }
+    public int? CategoryID { get; set; }
+    public Category? Category { get; set; }
     [Required]
     public string Title { get; set; }
     [Required]
     public int Year { get; set; }
+    public string? Director { get; set; }
+    public Rating? Rating { get; set; }
     [Required]
-    public string Director { get; set; }
-    [Required]
-    public Rating Rating { get; set; }
-
     public bool Edited { get; set; } = false;
-    public string? Lent_To { get; set; } // ? = null is acceptable
+    public string? Lent_To { get; set; }
+    [Required]
+    public bool CopiedToPlex { get; set; } = false;
     [MaxLength(25)]
     public string? Notes { get; set; }
     
